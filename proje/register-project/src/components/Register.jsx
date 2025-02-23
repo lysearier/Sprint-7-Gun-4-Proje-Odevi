4744f0a8dd9ca6541bfb6838f199b998e1c6f909
Eren Gündüz <erengndz57@outlook.com>
2025-02-23 12:47:55 +0300

    Validation eklendi

--- proje/register-project/src/components/Register.jsx
+++ proje/register-project/src/components/Register.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Form, Label, Input, FormGroup, Button, Card, CardBody, CardHeader} from 'reactstrap';
+import { useEffect, useState } from 'react';
+import { Form, Label, Input, FormGroup, Button, Card, CardBody, CardHeader, FormFeedback} from 'reactstrap';
 
 const initialValues = {
   ad: '',
@@ -24,6 +24,23 @@ export default function Register() {
   })
   const [isValid, setIsValid] = useState(false);
 
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+  let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+
+  useEffect(() => {
+    if(formData.ad.trim().length >= 3 && formData.soyad.trim().length >= 3 && validateEmail(formData.email) && regex.test(formData.password)) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }, [formData])
+
   const handleChange = ((event) => {
     const {name, value} = event.target;
     setFormData({...formData, [name]: value})
@@ -35,16 +52,24 @@ export default function Register() {
       }
     }
     if(name ==='email') {
-
+      if(validateEmail(value)) {
+        setErrors({...errors, [name]: false})
+      } else {
+        setErrors({...errors, [name]: true})
+      }
     }
     if (name ==='password') {
-      
+      if(regex.test(value)) {
+        setErrors({...errors, [name]: false})
+      } else {
+        setErrors({...errors, [name]: true}) 
+      }
     }
   })
 
   const handleSubmit = ((event) => {
     event.preventDefault();
-
+    if (!isValid) return;
   }) 
 
 
@@ -66,7 +91,9 @@ export default function Register() {
       type="text"
       onChange={handleChange}
       value = {formData.ad}
+      invalid = {errors.ad}
     />
+    {errors.ad && <FormFeedback>{errorMessages.ad}</FormFeedback>}
   </FormGroup>
   <FormGroup>
     <Label for="soyad">
@@ -79,7 +106,9 @@ export default function Register() {
       type="text"
       onChange={handleChange}
       value = {formData.soyad}
+      invalid = {errors.soyad}
     />
+    {errors.soyad && <FormFeedback>{errorMessages.soyad}</FormFeedback>}
   </FormGroup>
   <FormGroup>
     <Label for="email">
@@ -92,7 +121,9 @@ export default function Register() {
       type="email"
       onChange={handleChange}
       value = {formData.email}
+      invalid = {errors.email}
     />
+    {errors.email && <FormFeedback>{errorMessages.email}</FormFeedback>}
   </FormGroup>
   <FormGroup>
     <Label for="password">
@@ -105,10 +136,12 @@ export default function Register() {
       type="password"
       onChange={handleChange}
       value = {formData.password}
+      invalid = {errors.password}
     />
+    {errors.password && <FormFeedback>{errorMessages.password}</FormFeedback>}
   </FormGroup>
 
-  <Button onChange={handleSubmit}>
+  <Button onChange={handleSubmit} disabled = {!isValid}>
     Submit
   </Button>
 </Form>
